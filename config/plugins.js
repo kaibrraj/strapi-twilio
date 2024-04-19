e87b1a4b744060c0bcf679891209a0ee14a4be00
Ravi Chaudary <ravi@ouverture.ai>
2024-04-19 14:54:30 +0530

    new changes

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
     "vercel-deploy": {
       enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_QlTNOTYWkSVCXqesdpCiXthcRzuR/gaBLwOLUUG",
+        apiToken: "QHgJbEVfm5PuDX57llCZGpMA",
+        appFilter: "strapi-twilio",
+        teamFilter: "tom-bits-projects",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
     },
   });
